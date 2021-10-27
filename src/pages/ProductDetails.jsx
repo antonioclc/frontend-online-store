adceb164072ff2bfe34cf4243cc864c89390b529
Antônio Campos <aclcampos@live.com>
2021-10-27 12:48:15 -0300

    Grupo 9 - Requisito 9 completo

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CartButton from '../components/CartButton';
 
 export default class ProductDetails extends React.Component {
   render() {
-    const { location: { state: { title, thumbnail, price } } } = this.props;
+    const { location: { state: 
+      { title, thumbnail, price, id } }, addToCart, cartList } = this.props;
     return (
       <div data-testid="product">
+        <CartButton cartList={ cartList } />
         <h3 data-testid="product-detail-name">
           { title }
         </h3>
@@ -13,6 +16,13 @@ export default class ProductDetails extends React.Component {
         <h2>
           { price }
         </h2>
+        <input
+          id={ id }
+          type="button"
+          value="Adicionar item"
+          data-testid="product-detail-add-to-cart"
+          onClick={ addToCart }
+        />
       </div>
     );
   }
