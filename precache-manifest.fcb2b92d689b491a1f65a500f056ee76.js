9322585bc4506f032d63846a23728abc9884c576
Antônio <aclcampos@live.com>
2022-01-09 00:39:12 -0300

    Updates

---
+++ precache-manifest.fcb2b92d689b491a1f65a500f056ee76.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c103053c84fb75b1ea007c40873179",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "c4e40c3d72849ea93be8",
+    "url": "/frontend-online-store/static/css/main.ccdf8cce.chunk.css"
+  },
+  {
+    "revision": "c6a27354f56c2c21d1c1",
+    "url": "/frontend-online-store/static/js/2.61cac2f2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/frontend-online-store/static/js/2.61cac2f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e40c3d72849ea93be8",
+    "url": "/frontend-online-store/static/js/main.d43f52d6.chunk.js"
+  },
+  {
+    "revision": "582737c6385ae9ed11b6",
+    "url": "/frontend-online-store/static/js/runtime-main.720530ed.js"
+  },
+  {
+    "revision": "292cf4440f12264e7d7ae44ea14ea0f3",
+    "url": "/frontend-online-store/static/media/carrinho.292cf444.png"
+  },
+  {
+    "revision": "494eb90f8c7f427df8dac6c02e208c0a",
+    "url": "/frontend-online-store/static/media/title.494eb90f.png"
+  }
+]);
